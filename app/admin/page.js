3f80a2a954c6ae4dc5b04c1d970c422def0abd9f
Gary <squeakyclean.gg@gmail.com>
2026-09-05 09:08:26 +0100

    Update Supabase URL for testing purposes
    Replaced Supabase URL with a hardcoded value for testing.

--- app/admin/page.js
+++ app/admin/page.js
@@ -4,7 +4,7 @@ export default async function Admin() {
   let message = "Testing Supabase connection...";
 
   try {
-    const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+    const url = "https://bwfzxcvwaiipurwdtaxv.supabase.co";
     const key = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
     if (!url || !key) {
