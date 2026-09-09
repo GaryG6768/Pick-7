c96bb3954584c35de6eedadd7760dbc204a6b824
Gary <squeakyclean.gg@gmail.com>
2026-09-09 22:54:12 +0100

    Add service worker for caching and fetch handling

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+const VERSION = "pick7-v1";
+
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== VERSION)
+          .map((key) => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
+});
