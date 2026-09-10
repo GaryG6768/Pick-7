ffa0f0259dbee16e04b5622867d1091bfcc98ac0
Gary <squeakyclean.gg@gmail.com>
2026-09-10 11:58:34 +0100

    Fix import path for supabase in Nav.js

--- app/Nav.js
+++ app/Nav.js
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { supabase } from "./lib/supabase";
+import { supabase } from "../lib/supabase";
 
 export default function Nav() {
   const [canViewPicks, setCanViewPicks] = useState(false);
